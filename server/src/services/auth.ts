0593d5c5717bc61df4fd7b5471e738323c8011c0
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-11-04 17:27:33 -0500

    modified/refactored to now work with graphql ; ignores login and addUser/signup operations

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,33 +1,40 @@
-import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { GraphQLError } from 'graphql';
 import dotenv from 'dotenv';
 dotenv.config();
 
-interface JwtPayload {
-  _id: unknown;
-  username: string;
-  email: string,
-}
+const operationsToSkipAuthentication = ['login', 'addUser']; // token not need for these operations
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export const authenticateToken = ({ req }: any) => {
+  if (req.body && req.body.operationName) {
+    const operationName = req.body.operationName;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
+    // Skip authentication if not needed
+    if (operationsToSkipAuthentication.includes(operationName)) {
+      return {};
+    }
+  }
 
-    const secretKey = process.env.JWT_SECRET_KEY || '';
+  let token = req.body.token || req.query.token || req.headers.authorization;
 
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
 
-      req.user = user as JwtPayload;
-      return next();
+  if (!token) {
+    throw new GraphQLError("Authorization token missing", {
+      extensions: { code: 'UNAUTHENTICATED' },
     });
-  } else {
-    res.sendStatus(401); // Unauthorized
+  }
+
+  try {
+    const { data }: any = jwt.verify(token, process.env.JWT_SECRET_KEY || '', { maxAge: '1hr' });
+    return { user: data };
+  } catch (err) {
+    console.log(`Invalid token: ${err}`);
+    throw new GraphQLError("Invalid or expired token", {
+      extensions: { code: 'UNAUTHENTICATED' },
+    });
   }
 };
 
